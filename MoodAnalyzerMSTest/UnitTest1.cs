cd0e2cc1d86284bd99526a9b673302344920e48c
Vivek kumar <viveksinghssm496@gmail.com>
2022-06-12 11:47:39 +0530

    Added to FieldUsingConstructor

--- MoodAnalyzerMSTest/UnitTest1.cs
+++ MoodAnalyzerMSTest/UnitTest1.cs
@@ -113,5 +113,12 @@ namespace MoodAnalyzerMSTest
             string actual = MoodAnalyzerFactory.InvokeAnalyzeMood("HAPPY", "AnalyzeMood");
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void GivenHAPPYMessageWithReflectorShouldReturnHAPPY()
+        {
+            string result = MoodAnalyzerFactory.SetField("HAPPY", "message");
+            Assert.AreEqual("HAPPY", result);
+        }
     }
 }
