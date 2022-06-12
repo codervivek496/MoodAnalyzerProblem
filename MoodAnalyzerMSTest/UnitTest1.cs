4ee0670ac4b75088d5e62db7b7479632ec4679e3
Vivek kumar <viveksinghssm496@gmail.com>
2022-06-12 10:57:30 +0530

    Added to CustomException

--- MoodAnalyzerMSTest/UnitTest1.cs
+++ MoodAnalyzerMSTest/UnitTest1.cs
@@ -1,6 +1,7 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MoodAnalyzerProblem;
+
 
 namespace MoodAnalyzerMSTest
 {
@@ -54,6 +55,38 @@ namespace MoodAnalyzerMSTest
                 //Assert
                 Assert.AreEqual(expected, actual);
 
+            }
+        }
+
+        //Test Case 3.1
+        [TestMethod]
+        public void GivenNullMoodShouldThrowMoodAnalysisException()
+        {
+            try
+            {
+                string message = null;
+                MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
+                string actual = moodAnalyzer.AnalyzeMood();
+            }
+            catch (MoodAnalyzerCustomException ex)
+            {
+                Assert.AreEqual("Mood should not be null", ex.Message);
+            }
+        }
+
+        //Test Case 3.2
+        [TestMethod]
+        public void GivenEmptyMoodShouldThrowMoodAnalysisException()
+        {
+            try
+            {
+                string message = "";
+                MoodAnalyzer moodAnalyzer = new MoodAnalyzer(message);
+                string actual = moodAnalyzer.AnalyzeMood();
+            }
+            catch (MoodAnalyzerCustomException ex)
+            {
+                Assert.AreEqual("Mood should not be empty", ex.Message);
             }
         }
     }
