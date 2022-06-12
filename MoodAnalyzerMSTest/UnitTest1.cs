b3c7318e5827a67076821d9dbd6c9cf67af03709
Vivek kumar <viveksinghssm496@gmail.com>
2022-06-12 11:39:53 +0530

    Added to ReflectionToInvokeMethod

--- MoodAnalyzerMSTest/UnitTest1.cs
+++ MoodAnalyzerMSTest/UnitTest1.cs
@@ -105,5 +105,13 @@ namespace MoodAnalyzerMSTest
             object obj = MoodAnalyzerFactory.CreatemoodAnalyzeUsingParameterizedConstructor("MoodAnalyzerProblem.MoodAnalyzer", "MoodAnalyzer", "HAPPY");
             expected.Equals(obj);
         }
+
+        [TestMethod]
+        public void GivenHappyMoodShouldReturnHappy()
+        {
+            string expected = "HAPPY";
+            string actual = MoodAnalyzerFactory.InvokeAnalyzeMood("HAPPY", "AnalyzeMood");
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
